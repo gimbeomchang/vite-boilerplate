ee183a427067db81ed56c8235efa0fc11bdbfcb0
gimbeomchang <beomchang_kim@tmax.co.kr>
2023-09-13 12:50:49 +0900

    [config] env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [react(), tsconfigPaths()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.SERVER_BASE_URL,
+                    secure: false,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
+            },
+        },
+    };
 });
